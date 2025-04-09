cb9ff4a715ac2aa1882587e493ec4cccaffb76be
JavierValenzuelaEsparza <javier.valenzuela@g-global.com>
2025-04-08 17:01:10 -0700

    feat(receiveOrders): edit receive orders and immex validation

--- src/components/Dashboard/IncidentsCharts/Tendencies/MonthlyEvolution.js
+++ src/components/Dashboard/IncidentsCharts/Tendencies/MonthlyEvolution.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Card } from 'antd';
 import {
-  LineChart,
-  Line,
+  BarChart,
+  Bar,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -13,7 +13,7 @@ import {
 import { useIncidents } from '../../../../hooks/IncidentsHooks/Incidents.hooks';
 
 const MonthlyEvolutionChart = () => {
-  const { monthlyEvolution} = useIncidents();
+  const { monthlyEvolution } = useIncidents();
 
   const monthlyData = Object.keys(monthlyEvolution).map((month) => ({
     month,
@@ -24,21 +24,15 @@ const MonthlyEvolutionChart = () => {
   return (
     <Card title="Evolución Mensual" className="chart-card">
       <ResponsiveContainer width="100%" height={300}>
-        <LineChart data={monthlyData}>
+        <BarChart data={monthlyData}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="month" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line
-            type="monotone"
-            dataKey="total"
-            name="Total Incidencias"
-            stroke="#1890ff"
-            activeDot={{ r: 8 }}
-          />
-          <Line type="monotone" dataKey="finalizados" name="Finalizadas" stroke="#52c41a" />
-        </LineChart>
+          <Bar dataKey="total" name="Total Incidencias" fill="#1890ff" />
+          <Bar dataKey="finalizados" name="Finalizadas" fill="#52c41a" />
+        </BarChart>
       </ResponsiveContainer>
     </Card>
   );
